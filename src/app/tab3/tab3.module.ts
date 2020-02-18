97d4dc60eac1b851c87116065b59798df3a8688e
ZairiM <zairimarionpineda@gmail.com>
2020-02-17 21:33:39 -0500

    commitcito

--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -5,12 +5,16 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { IonicStorageModule } from '@ionic/storage';
+
+
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    IonicStorageModule.forRoot(),
     ExploreContainerComponentModule,
     RouterModule.forChild([{ path: '', component: Tab3Page }])
   ],
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { AlertController } from '@ionic/angular';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +11,48 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(public storage: Storage, public alertController: AlertController, public camera: Camera) {}
 
+  user:any;
+
+  guardar(){
+    this.storage.set('us', { name:'Marion', lastname:'Pineda', phone:'+57 123456789'});
+  }
+
+  leer(){
+    this.storage.get('us').then((val) => {
+      console.log(val);
+      this.user = val;
+    }).catch((error)=>{
+      console.log(error);
+    });
+    this.presentAlertMultipleButtons();
+  }
+
+  async presentAlertMultipleButtons() {
+    const alert = await this.alertController.create({
+      header: 'Usuario',
+      subHeader: 'Los datos del usuario son:',
+      message: this.user.name +"-" +this.user.lastname+"-"+this.user.phone,
+      buttons: ['Cancelar','Guardar']
+    });
+
+    await alert.present();
+  }
+
+  tomarCamara(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+     }, (err) => {
+      // Handle error
+     });
+  }
 }
